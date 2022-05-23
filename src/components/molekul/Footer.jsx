979b99f040b367e1854fc58ff51744471d6925dc
rivopelu <contactrivopelu@gmail.com>
2022-05-23 22:56:38 +0800

    masuk ke footer

---
+++ src/components/molekul/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="fLists">
+                <ul className="flist">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Region</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/components/molekul/index.js
+++ src/components/molekul/index.js
@@ -3,5 +3,6 @@ import HeaderComp from "./HeaderComp";
 import Featured from "./Featured";
 import PropertyList from "./PropertyList";
 import FeaturedProperties from "./FeaturedProperties";
+import Footer from "./Footer";
 
-export { NavbarComp, HeaderComp, Featured, PropertyList, FeaturedProperties }
+export { NavbarComp, Footer, HeaderComp, Featured, PropertyList, FeaturedProperties }
